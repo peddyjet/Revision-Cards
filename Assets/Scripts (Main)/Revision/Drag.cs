61626ad177b4c28b820b56e11b5e07f29334e298
lewisjet <lewisjet11@outlook.com>
2020-11-04 16:23:44 +0000

    Fixed Premature Dragging

--- Assets/Scripts (Main)/Revision/Drag.cs
+++ Assets/Scripts (Main)/Revision/Drag.cs
@@ -6,14 +6,14 @@ using Unity.Mathematics;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
-public class Drag : MonoBehaviour, IDragHandler, IEndDragHandler, IPointerClickHandler
+public class Drag : MonoBehaviour, IPointerClickHandler
 {
 
     public KeyValuePair<QuestionCard, Subject> keyValuePair;
     public string question;
     public string answer;
     public string subject;
-       
+
 
     private Vector3 panelLocation;
     [SerializeField] float percentThreshold = 0.75f;
@@ -24,10 +24,17 @@ public class Drag : MonoBehaviour, IDragHandler, IEndDragHandler, IPointerClickH
     [SerializeField] TextMeshProUGUI subjectA = null;
     [SerializeField] TextMeshProUGUI answerA = null;
 
+    [SerializeField] DragR dragR = null;
+
 
     new RectTransform transform;
     bool flippedOver = false;
     // Start is called before the first frame update
+
+    private void Awake()
+    {
+        dragR.enabled = false;
+    }
     void Start()
     {
         questionQ.text = question;
@@ -44,65 +51,7 @@ public class Drag : MonoBehaviour, IDragHandler, IEndDragHandler, IPointerClickH
        
     }
 
-    public void OnDrag(PointerEventData eventData)
-    {
-        if (!flippedOver) { return; }
-        Debug.Log(1);
-        float difference = eventData.pressPosition.x - eventData.position.x;
-        transform.position = panelLocation - new Vector3(difference, 0, 0);
-    }
-
-    public void OnEndDrag(PointerEventData eventData)
-    {
-        if (!flippedOver) { return; }
-        Debug.Log(2);
-        float percentage = (eventData.pressPosition.x - eventData.position.x) / 3;
-        if (Mathf.Abs(percentage) >= percentThreshold)
-        {
-            bool isCorrect = false;
-            Vector3 newLocation = panelLocation;
-            if (percentage > 0)
-            {
-                isCorrect = true;
-                newLocation += new Vector3(-3000, 0, 0);
-            }
-            else if(percentage < 0)
-            {
-                newLocation += new Vector3(3000, 0, 0);
-            }
-
-            StartCoroutine(SmoothMove(transform.position, newLocation, easing, ()
-                => { if (isCorrect && flippedOver) { FindObjectOfType<TestMGR>().Right(this); }
-                    else if (flippedOver) { FindObjectOfType<TestMGR>().Wrong(this); } else { return; } }));
-            panelLocation = newLocation;
-        }
-        else
-        {
-            StartCoroutine(SmoothMove( transform.position, panelLocation, easing));
-        }
-    }
-
-    IEnumerator SmoothMove(Vector3 startPos, Vector3 endPos, float seconds)
-    {
-        float t = 0f;
-        while (t <= 1.0)
-        {
-            t += Time.deltaTime / seconds;
-            transform.position = Vector3.Lerp(startPos, endPos, Mathf.SmoothStep(0f, 1f, t));
-            yield return null;
-        }
-    }
-    IEnumerator SmoothMove(Vector3 startPos, Vector3 endPos, float seconds,Action post_event)
-    {
-        float t = 0f;
-        while (t <= 1.0)
-        {
-            t += Time.deltaTime / seconds;
-            transform.position = Vector3.Lerp(startPos, endPos, Mathf.SmoothStep(0f, 1f, t));
-            yield return null;
-            post_event.Invoke();
-        }
-    }
+   
     IEnumerator SmoothRotate(float seconds, Action post_event, Action mid_event)
     {
         float t = 0f;
@@ -120,6 +69,7 @@ public class Drag : MonoBehaviour, IDragHandler, IEndDragHandler, IPointerClickH
     {
         if (flippedOver) { return; }
         flippedOver = true;
+       
         StartCoroutine(SmoothRotate( 0.25f, () => 
         {
             subjectQ.gameObject.SetActive(true);
@@ -131,6 +81,8 @@ public class Drag : MonoBehaviour, IDragHandler, IEndDragHandler, IPointerClickH
             questionQ.text = answer;
 
             transform.Rotate(180, 0, 0);
+            dragR.enabled = true;
+            
         },
             () => {
                 subjectQ.gameObject.SetActive(false);
---
+++ Assets/Scripts (Main)/Revision/DragR.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class DragR : MonoBehaviour, IDragHandler, IEndDragHandler
+{
+    private Vector3 panelLocation;
+    [SerializeField] float percentThreshold = 0.75f;
+    [SerializeField] float easing = 0.5f;
+
+    new RectTransform transform;
+
+    private void Awake()
+    {
+        transform = GetComponent<RectTransform>();
+        panelLocation = transform.position;
+    }
+    public void OnDrag(PointerEventData eventData)
+    {
+       
+            Debug.Log(1);
+            float difference = eventData.pressPosition.x - eventData.position.x;
+            transform.position = panelLocation - new Vector3(difference, 0, 0);
+        
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+       
+            Debug.Log(2);
+            float percentage = (eventData.pressPosition.x - eventData.position.x) / 3;
+            if (Mathf.Abs(percentage) >= percentThreshold)
+            {
+                bool isCorrect = false;
+                Vector3 newLocation = panelLocation;
+                if (percentage > 0)
+                {
+                    isCorrect = true;
+                    newLocation += new Vector3(-3000, 0, 0);
+                }
+                else if (percentage < 0)
+                {
+                    newLocation += new Vector3(3000, 0, 0);
+                }
+
+                StartCoroutine(SmoothMove(transform.position, newLocation, easing, ()
+                    =>
+                {
+                    if (isCorrect) { FindObjectOfType<TestMGR>().Right(GetComponent<Drag>()); }
+                    else { FindObjectOfType<TestMGR>().Wrong(GetComponent<Drag>()); }
+                }));
+                panelLocation = newLocation;
+            }
+            else
+            {
+                StartCoroutine(SmoothMove(transform.position, panelLocation, easing));
+            }
+        
+    }
+
+    IEnumerator SmoothMove(Vector3 startPos, Vector3 endPos, float seconds)
+    {
+        float t = 0f;
+        while (t <= 1.0)
+        {
+            t += Time.deltaTime / seconds;
+            transform.position = Vector3.Lerp(startPos, endPos, Mathf.SmoothStep(0f, 1f, t));
+            yield return null;
+        }
+    }
+    IEnumerator SmoothMove(Vector3 startPos, Vector3 endPos, float seconds, Action post_event)
+    {
+        float t = 0f;
+        while (t <= 1.0)
+        {
+            t += Time.deltaTime / seconds;
+            transform.position = Vector3.Lerp(startPos, endPos, Mathf.SmoothStep(0f, 1f, t));
+            yield return null;
+            post_event.Invoke();
+        }
+    }
+}
